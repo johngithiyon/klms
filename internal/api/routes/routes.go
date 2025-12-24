d6db4364be21f9ffa1ee5e88e22a4aa9a5df7f48
john githiyon <johngithiyon7@gmail.com>
2025-12-24 21:02:18 +0530

    Added routes for middleware

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -13,11 +13,11 @@ func Routes() {
 	http.HandleFunc("/otpverifypage",handlers.Otpverifypage)
 	http.HandleFunc("/loginpage",handlers.Loginpage)
 	http.HandleFunc("/userprofilepage",handlers.Userprofilepage)
-	http.HandleFunc("/coursespage",handlers.Coursespage)
-	http.HandleFunc("/videospage",handlers.Videospage)
-	http.HandleFunc("/videosuploadpage",handlers.Videouploadpage)
+	http.Handle("/coursespage",middleware.SessionMiddleware(http.HandlerFunc(handlers.Coursespage)))
+	http.Handle("/videospage",middleware.SessionMiddleware(http.HandlerFunc(handlers.Videospage)))
+	http.Handle("/videosuploadpage",middleware.SessionMiddleware(http.HandlerFunc(handlers.Videouploadpage)))
 	http.HandleFunc("/logoutpage",handlers.Logoutpage)
-	http.HandleFunc("/dashboardpage",handlers.Dashboardpage)
+	http.Handle("/dashboardpage",middleware.SessionMiddleware(http.HandlerFunc(handlers.Dashboardpage)))
 
 
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -29,7 +29,6 @@ func Routes() {
 	http.HandleFunc("/validemail",handlers.ValidEmail)
 	http.HandleFunc("/passotpverify",handlers.Passotpverify)
 	http.HandleFunc("/forgetpass",handlers.Forgetpassword)
-	http.HandleFunc("/session",middleware.SessionVerify) // To do: middleware want to implement 
 	http.HandleFunc("/userprofile",handlers.Userprofile)
 	http.HandleFunc("/deleteprofile",handlers.ProfileDelete)
 	http.HandleFunc("/role",handlers.Roles)
