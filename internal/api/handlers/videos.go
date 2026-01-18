6b0a2d1b65ee6d45337fcb5f71b1460b5a765232
john githiyon <johngithiyon7@gmail.com>
2026-01-18 23:39:06 +0530

    Added completed status

--- internal/api/handlers/videos.go
+++ internal/api/handlers/videos.go
@@ -1,22 +1,64 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
+	"klms/internal/api/errors"
 	"klms/internal/api/handlers/responses"
 	"klms/internal/api/models"
 	"klms/internal/api/storage/postgres"
+	"klms/internal/api/storage/redis"
+	"log"
 	"net/http"
 )
 
 
 func Videos(w http.ResponseWriter, r *http.Request) {
 
+
+			sessionid,cokkierr := r.Cookie("session-id")
+
+			if cokkierr != nil {
+				responses.JsonError(w,errors.Errcookie)
+				return 
+			}
+
+			username,rediserr:= redis.Redis.Get(r.Context(),sessionid.Value).Result()
+
+			if rediserr != nil {
+			 responses.JsonError(w,"Invalid Session Id")
+			 return
+	   }
+
 	           var v models.Videos
 			   var videos[]models.Videos
 
 			   id := r.URL.Query().Get("id")
 
-			   searchquery := "select video_title,video_description,video_url from course_videos where course_id=$1"
+			   searchquery := "select video_title,video_description,video_url from course_videos where course_id=$1"	   
+
+			   statusquery := "select status from course_progress where student_name=$1 and course_id=$2"
+
+			   statuscan,statusscanerr := postgres.Db.QueryContext(r.Context(),statusquery,username,id)
+
+			   if statusscanerr != nil {
+				if statusscanerr == sql.ErrNoRows {
+
+					v.Status = "not_started" 
+					log.Println("No course progress found")
+					return
+				}
+			
+				log.Println("DB error:", statusscanerr)
+				responses.JsonError(w, "Internal Server Error")
+				return
+			}
+
+			   for statuscan.Next() {
+				      
+				       statuscan.Scan(&v.Status)
+				     
+			   }
 
 			   rows,rowserr := postgres.Db.QueryContext(r.Context(),searchquery,id)
 
