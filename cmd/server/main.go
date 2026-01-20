6d7ef78ca266c55c1148a42b3352843cef2afa50
john githiyon <johngithiyon7@gmail.com>
2026-01-20 15:39:32 +0530

    Added default mux for global limit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"klms/internal/api/app"
+	"klms/internal/api/middleware"
 	"log"
 	"net/http"
 )
 
 func main() {
-     
 	app.Startup()
 	log.Println("Server is listening")
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080",middleware.Globalimit(http.DefaultServeMux))	
 }
